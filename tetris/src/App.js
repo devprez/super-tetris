0f672cd9a70f6f96eb11b6175b82f0ef5c0a25b2
Mycho <mycho@Love-Haight-Macs-2.local>
2020-04-15 12:49:40 -0700

    proj setup complete

---
+++ tetris/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Tetris Redux</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
